d81160795279038e2953fcf8a3c09d0f9cf5170f
Nanda Kishore <nandakishore97@icloud.com>
2025-10-10 17:56:55 +0530

    url issue fixed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  preview: {
+    host: true,
+    allowedHosts: ['gallant-connection-production.up.railway.app']
+  }
 })
